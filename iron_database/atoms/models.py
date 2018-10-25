a5bdfc8b494372d1ad84aa22bc9e51444b6dcd7c
renpj <openrpj@gmail.com>
2018-10-25 08:37:46 +0800

    initialize django project

---
+++ iron_database/atoms/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Atoms(models.Model):
+    ## copy from ase.db
+
+class Surface(Atoms):
+    # surface structure, indices, note
+    # 
+
+class Energy(models.Model): # maybe as mothod in Atoms
+
